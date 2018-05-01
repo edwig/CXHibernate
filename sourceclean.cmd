d1a00042a7e9a9e8362102bc4761e42d51bce4aa
Edwig Huisman <edwig.huisman@hetnet.nl>
2018-05-01 08:50:38 +0200

    Added sourcecleaning

---
+++ sourceclean.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+rmdir /s /q x64
+rmdir /s /q lib
+rmdir /s /q UnitTest\x64
+rmdir /s /q SQLComponents\Debug
+rmdir /s /q SQLComponents\Release
+rmdir /s /q SQLComponents\x64
+rmdir /s /q Marlin\Debug
+rmdir /s /q Marlin\Release
+rmdir /s /q Marlin\x64
+rmdir /s /q CXHibernate\x64
+rmdir /s /q CXHibernate\Debug
+rmdir /s /q CXHibernate\Release
